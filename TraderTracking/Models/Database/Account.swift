bf7a0c3a5555b76872ce03811c2d4c87c55c7955
Zach Bockskopf <zbockskopf@gmail.com>
2022-09-28 17:47:30 -0500

    Save

---
+++ TraderTracking/Models/Database/Account.swift
@@ -0,0 +1,15 @@
+//
+//  Account.swift
+//  TraderTracking
+//
+//  Created by Zach Bockskopf on 9/28/22.
+//
+
+import Foundation
+import RealmSwift
+
+//class Account: Object , ObjectKeyIdentifiable {
+//    @Persisted (primaryKey: true) var name: String
+//    @Persisted var balance: Double
+//    @Persisted var tickValue: Double
+//}
