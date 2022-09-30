b6e774e218f56c0fe08333de813282675a7ecff4
Zach Bockskopf <zbockskopf@users.noreply.github.com>
2022-09-29 23:43:00 -0500

    add account info
    Also made it so take profit and loss can be nil

--- TraderTracking/Models/Database/Account.swift
+++ TraderTracking/Models/Database/Account.swift
@@ -8,8 +8,10 @@
 import Foundation
 import RealmSwift
 
-//class Account: Object , ObjectKeyIdentifiable {
-//    @Persisted (primaryKey: true) var name: String
-//    @Persisted var balance: Double
-//    @Persisted var tickValue: Double
-//}
+class Account: Object , ObjectKeyIdentifiable {
+    @Persisted (primaryKey: true) var name: String
+    @Persisted var balance: Decimal
+    @Persisted var profitAndLoss: Decimal
+		@Persisted var fees: Decimal
+	
+}
